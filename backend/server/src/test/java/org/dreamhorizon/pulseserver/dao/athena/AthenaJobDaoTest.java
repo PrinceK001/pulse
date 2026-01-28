bc0db2440f4d8c9945b2455a2fa89aca5de855a8
Sarabjyot123 <sarabjyot.singh@dream11.com>
2026-01-28 20:01:58 +0530

    Fix athena test

--- backend/server/src/test/java/org/dreamhorizon/pulseserver/dao/athena/AthenaJobDaoTest.java
+++ backend/server/src/test/java/org/dreamhorizon/pulseserver/dao/athena/AthenaJobDaoTest.java
@@ -84,8 +84,9 @@ class AthenaJobDaoTest {
       verify(preparedQuery).rxExecute(tupleCaptor.capture());
       Tuple capturedTuple = tupleCaptor.getValue();
       assertThat(capturedTuple.getString(0)).isEqualTo(jobId);
-      assertThat(capturedTuple.getString(1)).isEqualTo(queryString);
-      assertThat(capturedTuple.getString(2)).isEqualTo(userEmail);
+      assertThat(capturedTuple.getString(1)).isNotNull(); // tenant_id
+      assertThat(capturedTuple.getString(2)).isEqualTo(queryString);
+      assertThat(capturedTuple.getString(3)).isEqualTo(userEmail);
     }
 
     @Test
@@ -127,6 +128,7 @@ class AthenaJobDaoTest {
       assertThat((Timestamp) capturedTuple.getValue(2)).isEqualTo(submissionDateTime);
       assertThat((Timestamp) capturedTuple.getValue(3)).isNotNull();
       assertThat(capturedTuple.getString(4)).isEqualTo(jobId);
+      assertThat(capturedTuple.getString(5)).isNotNull(); // tenant_id
     }
   }
 
@@ -171,6 +173,7 @@ class AthenaJobDaoTest {
       assertThat((Timestamp) capturedTuple.getValue(1)).isEqualTo(completionDateTime);
       assertThat((Timestamp) capturedTuple.getValue(2)).isNotNull();
       assertThat(capturedTuple.getString(3)).isEqualTo(jobId);
+      assertThat(capturedTuple.getString(4)).isNotNull(); // tenant_id
     }
   }
 
