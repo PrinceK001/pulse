5cd44a9aa16ae46d49dc6f1cb9b118d7447daee9
shubhamguptadream11 <shubham.gupta@dream11.com>
2026-02-09 15:28:17 +0530

    feat: added old arch module support

--- pulse-react-native-otel/example/ios/NativePulseExampleModule/RCTNativePulseExampleModule.mm
+++ pulse-react-native-otel/example/ios/NativePulseExampleModule/RCTNativePulseExampleModule.mm
@@ -1,8 +1,7 @@
 //
-//  RCTNativePulseExampleModule.m
-//  PulseReactNativeOtelExample
-//
-//  Created by Shubham Gupta on 09/12/25.
+//  RCTNativePulseExampleModule.mm
+//  New Arch: plain methods (no macros), TurboModule/spec calls them directly.
+//  Old Arch: RCT_EXPORT_METHOD so bridge discovers methods. Both use shared implementation.
 //
 
 #import "RCTNativePulseExampleModule.h"
@@ -18,149 +17,110 @@ RCT_EXPORT_MODULE(NativePulseExampleModule)
   return NO;
 }
 
-- (void)makeGetRequest:(NSString *)url
-               resolve:(RCTPromiseResolveBlock)resolve
-                reject:(RCTPromiseRejectBlock)reject
+#pragma mark - Shared implementation
+
+static NSDictionary *resultDictionaryFromResponse(NSHTTPURLResponse *httpResponse, NSData *data)
+{
+  NSString *responseBody = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+  if (!responseBody) responseBody = @"";
+  NSMutableDictionary *headers = [NSMutableDictionary dictionary];
+  [httpResponse.allHeaderFields enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+    NSString *headerName = [key isKindOfClass:[NSString class]] ? key : [key description];
+    NSString *headerValue = [obj isKindOfClass:[NSString class]] ? obj : [obj description];
+    id existingValue = headers[headerName];
+    if (existingValue) {
+      if ([existingValue isKindOfClass:[NSArray class]]) {
+        NSMutableArray *values = [existingValue mutableCopy];
+        [values addObject:headerValue];
+        headers[headerName] = values;
+      } else {
+        headers[headerName] = @[existingValue, headerValue];
+      }
+    } else {
+      headers[headerName] = headerValue;
+    }
+  }];
+  return @{ @"status": @(httpResponse.statusCode), @"body": responseBody, @"headers": headers };
+}
+
+- (void)doMakeGetRequest:(NSString *)url resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject
 {
   NSURL *requestURL = [NSURL URLWithString:url];
   if (!requestURL) {
     reject(@"INVALID_URL", @"Invalid URL provided", nil);
     return;
   }
-
-  NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-  NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
-  
-  NSURLRequest *request = [NSURLRequest requestWithURL:requestURL];
-  
-  NSURLSessionDataTask *task = [session dataTaskWithRequest:request
-                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-    if (error) {
-      reject(@"NETWORK_ERROR", error.localizedDescription, error);
-      return;
-    }
-    
+  NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+  NSURLSessionDataTask *task = [session dataTaskWithRequest:[NSURLRequest requestWithURL:requestURL]
+                                          completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    if (error) { reject(@"NETWORK_ERROR", error.localizedDescription, error); return; }
     NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-    if (!httpResponse) {
-      reject(@"INVALID_RESPONSE", @"Invalid HTTP response", nil);
-      return;
-    }
-    
-    NSString *responseBody = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    if (!responseBody) {
-      responseBody = @"";
-    }
-    
-    NSMutableDictionary *headers = [NSMutableDictionary dictionary];
-    [httpResponse.allHeaderFields enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-      NSString *headerName = [key isKindOfClass:[NSString class]] ? key : [key description];
-      NSString *headerValue = [obj isKindOfClass:[NSString class]] ? obj : [obj description];
-      
-      // Handle multiple values for the same header (convert to array)
-      id existingValue = headers[headerName];
-      if (existingValue) {
-        if ([existingValue isKindOfClass:[NSArray class]]) {
-          NSMutableArray *values = [existingValue mutableCopy];
-          [values addObject:headerValue];
-          headers[headerName] = values;
-        } else {
-          headers[headerName] = @[existingValue, headerValue];
-        }
-      } else {
-        headers[headerName] = headerValue;
-      }
-    }];
-    
-    NSDictionary *result = @{
-      @"status": @(httpResponse.statusCode),
-      @"body": responseBody,
-      @"headers": headers
-    };
-    
-    resolve(result);
+    if (!httpResponse) { reject(@"INVALID_RESPONSE", @"Invalid HTTP response", nil); return; }
+    resolve(resultDictionaryFromResponse(httpResponse, data));
   }];
-  
   [task resume];
 }
 
-- (void)makePostRequest:(NSString *)url
-                    body:(NSString *)body
-                 resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject
+- (void)doMakePostRequest:(NSString *)url body:(NSString *)body resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject
 {
   NSURL *requestURL = [NSURL URLWithString:url];
   if (!requestURL) {
     reject(@"INVALID_URL", @"Invalid URL provided", nil);
     return;
   }
-
-  NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-  NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
-  
   NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:requestURL];
   [request setHTTPMethod:@"POST"];
   [request setValue:@"application/json; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-  
-  if (body) {
-    NSData *bodyData = [body dataUsingEncoding:NSUTF8StringEncoding];
-    [request setHTTPBody:bodyData];
-  }
-  
+  if (body) [request setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
+  NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
   NSURLSessionDataTask *task = [session dataTaskWithRequest:request
-                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-    if (error) {
-      reject(@"NETWORK_ERROR", error.localizedDescription, error);
-      return;
-    }
-    
+                                          completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    if (error) { reject(@"NETWORK_ERROR", error.localizedDescription, error); return; }
     NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-    if (!httpResponse) {
-      reject(@"INVALID_RESPONSE", @"Invalid HTTP response", nil);
-      return;
-    }
-    
-    NSString *responseBody = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    if (!responseBody) {
-      responseBody = @"";
-    }
-    
-    NSMutableDictionary *headers = [NSMutableDictionary dictionary];
-    [httpResponse.allHeaderFields enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-      NSString *headerName = [key isKindOfClass:[NSString class]] ? key : [key description];
-      NSString *headerValue = [obj isKindOfClass:[NSString class]] ? obj : [obj description];
-      
-      // Handle multiple values for the same header (convert to array)
-      id existingValue = headers[headerName];
-      if (existingValue) {
-        if ([existingValue isKindOfClass:[NSArray class]]) {
-          NSMutableArray *values = [existingValue mutableCopy];
-          [values addObject:headerValue];
-          headers[headerName] = values;
-        } else {
-          headers[headerName] = @[existingValue, headerValue];
-        }
-      } else {
-        headers[headerName] = headerValue;
-      }
-    }];
-    
-    NSDictionary *result = @{
-      @"status": @(httpResponse.statusCode),
-      @"body": responseBody,
-      @"headers": headers
-    };
-    
-    resolve(result);
+    if (!httpResponse) { reject(@"INVALID_RESPONSE", @"Invalid HTTP response", nil); return; }
+    resolve(resultDictionaryFromResponse(httpResponse, data));
   }];
-  
   [task resume];
 }
 
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
+#pragma mark - New Architecture (no macros)
+
+#if RCT_NEW_ARCH_ENABLED
+
+- (void)makeGetRequest:(NSString *)url resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject
 {
-    return std::make_shared<facebook::react::NativePulseExampleModuleSpecJSI>(params);
+  [self doMakeGetRequest:url resolve:resolve reject:reject];
 }
 
-@end
+- (void)makePostRequest:(NSString *)url body:(NSString *)body resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject
+{
+  [self doMakePostRequest:url body:body resolve:resolve reject:reject];
+}
 
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params
+{
+  return std::make_shared<facebook::react::NativePulseExampleModuleSpecJSI>(params);
+}
+
+#pragma mark - Old Architecture (RCT_EXPORT_METHOD)
+
+#else
+
+RCT_EXPORT_METHOD(makeGetRequest:(NSString *)url
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+  [self doMakeGetRequest:url resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(makePostRequest:(NSString *)url
+                  body:(NSString *)body
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+  [self doMakePostRequest:url body:body resolve:resolve reject:reject];
+}
+
+#endif
+
+@end
--- pulse-react-native-otel/ios/PulseReactNativeOtel.mm
+++ pulse-react-native-otel/ios/PulseReactNativeOtel.mm
@@ -15,110 +15,182 @@ RCT_EXPORT_MODULE()
   return NO;
 }
 
-- (NSNumber *)isInitialized
+#pragma mark - Shared implementation (used by both arches)
+
+- (NSNumber *)doIsInitialized
 {
   return @([PulseSDK isSDKInitialized]);
 }
 
-- (NSNumber *)trackEvent:(NSString *)event
-          observedTimeMs:(double)observedTimeMs
-              properties:(NSDictionary *)properties
+- (NSNumber *)doTrackEvent:(NSString *)event observedTimeMs:(double)observedTimeMs properties:(NSDictionary *)properties
 {
-  [PulseReactNativeOtelLogger trackEvent:event
-                        observedTimeMs:observedTimeMs
-                            properties:properties];
+  [PulseReactNativeOtelLogger trackEvent:event observedTimeMs:observedTimeMs properties:properties];
   return @YES;
 }
 
-- (NSString *)startSpan:(NSString *)name
-             inheritContext:(NSNumber *)inheritContext
-             attributes:(NSDictionary *)attributes
+- (NSString *)doStartSpan:(NSString *)name inheritContext:(NSNumber *)inheritContext attributes:(NSDictionary *)attributes
 {
   return [PulseReactNativeOtelTracer startSpan:name attributes:attributes];
 }
 
-- (NSNumber *)endSpan:(NSString *)spanId
-           statusCode:(NSString *)statusCode
+- (NSNumber *)doEndSpan:(NSString *)spanId statusCode:(NSString *)statusCode
 {
   [PulseReactNativeOtelTracer endSpan:spanId statusCode:statusCode];
   return @YES;
 }
 
-- (NSNumber *)addSpanEvent:(NSString *)spanId
-                name:(NSString *)name
-          attributes:(NSDictionary *)attributes
+- (NSNumber *)doAddSpanEvent:(NSString *)spanId name:(NSString *)name attributes:(NSDictionary *)attributes
 {
   [PulseReactNativeOtelTracer addEvent:spanId name:name attributes:attributes];
   return @YES;
 }
 
-- (NSNumber *)setSpanAttributes:(NSString *)spanId
-               attributes:(NSDictionary *)attributes
+- (NSNumber *)doSetSpanAttributes:(NSString *)spanId attributes:(NSDictionary *)attributes
 {
   [PulseReactNativeOtelTracer setAttributes:spanId attributes:attributes];
   return @YES;
 }
 
-- (NSNumber *)recordSpanException:(NSString *)spanId
-               errorMessage:(NSString *)errorMessage
-                 stackTrace:(NSString *)stackTrace
+- (NSNumber *)doRecordSpanException:(NSString *)spanId errorMessage:(NSString *)errorMessage stackTrace:(NSString *)stackTrace
 {
   [PulseReactNativeOtelTracer recordException:spanId errorMessage:errorMessage stackTrace:stackTrace];
   return @YES;
 }
 
-- (nonnull NSNumber *)reportException:(nonnull NSString *)errorMessage 
-                        observedTimeMs:(double)observedTimeMs 
-                            stackTrace:(nonnull NSString *)stackTrace 
-                              isFatal:(BOOL)isFatal 
-                            errorType:(nonnull NSString *)errorType 
-                           attributes:(nonnull NSDictionary *)attributes
-{ 
-  [PulseReactNativeOtelLogger reportException:errorMessage
-                                observedTimeMs:observedTimeMs
-                                    stackTrace:stackTrace
-                                      isFatal:isFatal
-                                    errorType:errorType
-                                   attributes:attributes];
+- (NSNumber *)doReportException:(NSString *)errorMessage observedTimeMs:(double)observedTimeMs stackTrace:(NSString *)stackTrace isFatal:(BOOL)isFatal errorType:(NSString *)errorType attributes:(NSDictionary *)attributes
+{
+  [PulseReactNativeOtelLogger reportException:errorMessage observedTimeMs:observedTimeMs stackTrace:stackTrace isFatal:isFatal errorType:errorType attributes:attributes];
   return @YES;
 }
-// Discard span (no-op on iOS)
-- (NSNumber *)discardSpan:(NSString *)spanId
+
+- (NSNumber *)doDiscardSpan:(NSString *)spanId
 {
   return @NO;
 }
 
-- (void)setUserId:(NSString * _Nullable)id { 
+- (void)doSetUserId:(NSString *)id
+{
   [PulseSDK setUserId:id];
 }
 
-- (void)setUserProperties:(nonnull NSDictionary *)properties { 
-  NSDictionary<NSString *, PulseAttributeValue *> *convertedProperties = [AttributeValueConverter convertFromDictionary:properties];
-  [PulseSDK setUserProperties:convertedProperties];
+- (void)doSetUserProperties:(NSDictionary *)properties
+{
+  NSDictionary<NSString *, PulseAttributeValue *> *converted = [AttributeValueConverter convertFromDictionary:properties];
+  [PulseSDK setUserProperties:converted];
 }
 
-- (void)setUserProperty:(nonnull NSString *)name value:(NSString * _Nullable)value { 
+- (void)doSetUserProperty:(NSString *)name value:(NSString *)value
+{
   PulseAttributeValue *attrValue = value ? [PulseAttributeValue attributeValueFromValue:value] : nil;
   [PulseSDK setUserProperty:name value:attrValue];
 }
 
-- (void)triggerAnr {
-}
-
-- (NSNumber *)setCurrentScreenName:(NSString *)screenName {
+- (NSNumber *)doSetCurrentScreenName:(NSString *)screenName
+{
   [ReactNativeScreenNameTracker setCurrentScreenName:screenName];
   return @YES;
 }
 
-- (NSDictionary * _Nullable)getAllFeatures {
-  // TODO: Implement actual feature flag retrieval from SDK config
+- (NSDictionary *)doGetAllFeatures
+{
   return nil;
 }
 
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
+#pragma mark - New Architecture (no macros; TurboModule/spec calls these directly)
+
+#if RCT_NEW_ARCH_ENABLED
+
+- (NSNumber *)isInitialized { return [self doIsInitialized]; }
+
+- (NSNumber *)trackEvent:(NSString *)event observedTimeMs:(double)observedTimeMs properties:(NSDictionary *)properties
+{ return [self doTrackEvent:event observedTimeMs:observedTimeMs properties:properties]; }
+
+- (NSString *)startSpan:(NSString *)name inheritContext:(NSNumber *)inheritContext attributes:(NSDictionary *)attributes
+{ return [self doStartSpan:name inheritContext:inheritContext attributes:attributes]; }
+
+- (NSNumber *)endSpan:(NSString *)spanId statusCode:(NSString *)statusCode
+{ return [self doEndSpan:spanId statusCode:statusCode]; }
+
+- (NSNumber *)addSpanEvent:(NSString *)spanId name:(NSString *)name attributes:(NSDictionary *)attributes
+{ return [self doAddSpanEvent:spanId name:name attributes:attributes]; }
+
+- (NSNumber *)setSpanAttributes:(NSString *)spanId attributes:(NSDictionary *)attributes
+{ return [self doSetSpanAttributes:spanId attributes:attributes]; }
+
+- (NSNumber *)recordSpanException:(NSString *)spanId errorMessage:(NSString *)errorMessage stackTrace:(NSString *)stackTrace
+{ return [self doRecordSpanException:spanId errorMessage:errorMessage stackTrace:stackTrace]; }
+
+- (NSNumber *)reportException:(NSString *)errorMessage observedTimeMs:(double)observedTimeMs stackTrace:(NSString *)stackTrace isFatal:(BOOL)isFatal errorType:(NSString *)errorType attributes:(NSDictionary *)attributes
+{ return [self doReportException:errorMessage observedTimeMs:observedTimeMs stackTrace:stackTrace isFatal:isFatal errorType:errorType attributes:attributes]; }
+
+- (NSNumber *)discardSpan:(NSString *)spanId { return [self doDiscardSpan:spanId]; }
+
+- (void)setUserId:(NSString *)id { [self doSetUserId:id]; }
+
+- (void)setUserProperties:(NSDictionary *)properties { [self doSetUserProperties:properties]; }
+
+- (void)setUserProperty:(NSString *)name value:(NSString *)value { [self doSetUserProperty:name value:value]; }
+
+- (void)triggerAnr { }
+
+- (NSNumber *)setCurrentScreenName:(NSString *)screenName { return [self doSetCurrentScreenName:screenName]; }
+
+- (NSDictionary *)getAllFeatures { return [self doGetAllFeatures]; }
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params
 {
-    return std::make_shared<facebook::react::NativePulseReactNativeOtelSpecJSI>(params);
+  return std::make_shared<facebook::react::NativePulseReactNativeOtelSpecJSI>(params);
 }
+
+#pragma mark - Old Architecture (RCT_EXPORT_* so bridge discovers methods)
+
+#else
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(isInitialized)
+{ return [self doIsInitialized]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(trackEvent:(NSString *)event observedTimeMs:(double)observedTimeMs properties:(NSDictionary *)properties)
+{ return [self doTrackEvent:event observedTimeMs:observedTimeMs properties:properties]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(startSpan:(NSString *)name inheritContext:(NSNumber *)inheritContext attributes:(NSDictionary *)attributes)
+{ return [self doStartSpan:name inheritContext:inheritContext attributes:attributes]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(endSpan:(NSString *)spanId statusCode:(NSString *)statusCode)
+{ return [self doEndSpan:spanId statusCode:statusCode]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(addSpanEvent:(NSString *)spanId name:(NSString *)name attributes:(NSDictionary *)attributes)
+{ return [self doAddSpanEvent:spanId name:name attributes:attributes]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(setSpanAttributes:(NSString *)spanId attributes:(NSDictionary *)attributes)
+{ return [self doSetSpanAttributes:spanId attributes:attributes]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(recordSpanException:(NSString *)spanId errorMessage:(NSString *)errorMessage stackTrace:(NSString *)stackTrace)
+{ return [self doRecordSpanException:spanId errorMessage:errorMessage stackTrace:stackTrace]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(reportException:(NSString *)errorMessage observedTimeMs:(double)observedTimeMs stackTrace:(NSString *)stackTrace isFatal:(BOOL)isFatal errorType:(NSString *)errorType attributes:(NSDictionary *)attributes)
+{ return [self doReportException:errorMessage observedTimeMs:observedTimeMs stackTrace:stackTrace isFatal:isFatal errorType:errorType attributes:attributes]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(discardSpan:(NSString *)spanId)
+{ return [self doDiscardSpan:spanId]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(setCurrentScreenName:(NSString *)screenName)
+{ return [self doSetCurrentScreenName:screenName]; }
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getAllFeatures)
+{ return [self doGetAllFeatures]; }
+
+RCT_EXPORT_METHOD(setUserId:(NSString *)id)
+{ [self doSetUserId:id]; }
+
+RCT_EXPORT_METHOD(setUserProperties:(NSDictionary *)properties)
+{ [self doSetUserProperties:properties]; }
+
+RCT_EXPORT_METHOD(setUserProperty:(NSString *)name value:(NSString *)value)
+{ [self doSetUserProperty:name value:value]; }
+
+RCT_EXPORT_METHOD(triggerAnr)
+{ }
+
+#endif
 
 @end
